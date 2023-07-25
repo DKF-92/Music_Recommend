199884d740a66caf758caab1e0c1797976bcf183
DKF-92 <devin.ferko@outlook.com>
2023-07-25 11:13:07 +0100

    Added csv, start of TSNE model

--- music_recommender.py
+++ music_recommender.py
@@ -10,3 +10,22 @@ from sklearn.manifold import TSNE
 
 import warnings
 warnings.filterwarnings('ignore')
+
+# Reads dataset
+tracks = pd.read_csv('Music_Recommend\musicdataset.csv')
+print(tracks.head())
+print(tracks.shape)
+print(tracks.info())
+
+#checks for null values
+print(tracks.isnull().sum())
+
+#drops unneeded columns
+tracks = tracks.drop(['id'], axis=1)
+
+# use t-SNE for visualization of high dimensional data
+model = TSNE(n_components=2, random_state=0)
+tsne_data = model.fit()
+plt.figure(figsize= (7, 7))
+plt.scatter(tsne_data[:,0], tsne_data[:,1])
+plt.show()
