8e51b9d9862ea30eb8518f4f8fff139ee141063a
DKF-92 <devin.ferko@outlook.com>
2023-07-25 10:04:23 +0100

    Created recommender py file, import dependencies and libraries

---
+++ music_recommender.py
@@ -0,0 +1,12 @@
+# Import dependencies/libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb 
+
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.manifold import TSNE
+
+import warnings
+warnings.filterwarnings('ignore')
